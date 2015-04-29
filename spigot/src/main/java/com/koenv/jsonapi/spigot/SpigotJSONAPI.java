1fe037290d2976727004b1ec779b03aa5cdab1c3
Koen Vlaswinkel <koen@vlaswinkel.info>
2015-04-29 14:50:15 +0200

    Add first test of Spigot

--- spigot/src/main/java/com/koenv/jsonapi/spigot/SpigotJSONAPI.java
+++ spigot/src/main/java/com/koenv/jsonapi/spigot/SpigotJSONAPI.java
@@ -1,7 +1,68 @@
 package com.koenv.jsonapi.spigot;
 
-public class SpigotJSONAPI {
-	public void start() {
-		
-	}
+import com.koenv.jsonapi.methods.APIMethod;
+import com.koenv.jsonapi.methods.MethodInvocationException;
+import com.koenv.jsonapi.methods.MethodInvoker;
+import com.koenv.jsonapi.parser.MethodParser;
+import com.koenv.jsonapi.parser.ParseException;
+import com.koenv.jsonapi.parser.expressions.Expression;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SpigotJSONAPI extends JavaPlugin {
+    private MethodParser methodParser;
+    private MethodInvoker methodInvoker;
+
+    @Override
+    public void onEnable() {
+        methodParser = new MethodParser();
+        methodInvoker = new MethodInvoker();
+
+        methodInvoker.registerMethods(this);
+
+        getCommand("jsonapi").setExecutor((sender, command, label, args) -> {
+            if (args.length < 1) {
+                sender.sendMessage(ChatColor.RED + "This plugin needs at least 1 parameter.");
+                return true;
+            }
+            String subCommand = args[0];
+            if (subCommand.equals("execute") || subCommand.equals("exec")) {
+                if (args.length < 2) {
+                    sender.sendMessage(ChatColor.RED + "Please specify the command.");
+                    return true;
+                }
+                StringBuilder execStringBuilder = new StringBuilder();
+                for (int i = 0; i < args.length; i++) {
+                    if (i == 0) {
+                        continue;
+                    }
+                    execStringBuilder.append(args[i]);
+                    execStringBuilder.append(" ");
+                }
+                try {
+                    Expression expression = methodParser.parse(execStringBuilder.toString());
+                    Object result = methodInvoker.invokeMethod(expression);
+                    sender.sendMessage(ChatColor.GREEN + String.valueOf(result));
+                    return true;
+                } catch (ParseException | MethodInvocationException e) {
+                    sender.sendMessage(ChatColor.RED + "Failed to execute command: " + e.getMessage());
+                    e.printStackTrace();
+                    return true;
+                }
+            }
+            return false;
+        });
+    }
+
+    @APIMethod(namespace = "players")
+    public static Player getPlayer(String name) {
+        return Bukkit.getServer().getPlayer(name);
+    }
+
+    @APIMethod(operatesOn = Player.class)
+    public static String getUUID(Player self) {
+        return self.getUniqueId().toString();
+    }
 }
