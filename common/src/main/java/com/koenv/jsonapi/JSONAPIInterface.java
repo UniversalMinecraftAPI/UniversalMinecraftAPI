7806215456bb8290441b5f15101c7ee7be043f5b
Koen Vlaswinkel <koen@vlaswinkel.info>
2015-04-30 11:20:09 +0200

    Add link to Java 8 docs

--- common/src/main/java/com/koenv/jsonapi/JSONAPIInterface.java
+++ common/src/main/java/com/koenv/jsonapi/JSONAPIInterface.java
@@ -8,11 +8,23 @@ import com.koenv.jsonapi.parser.ExpressionParser;
  * The interface of the JSONAPI, so it can be used in tests.
  */
 public interface JSONAPIInterface {
+    /**
+     * Sets up the JSONAPIInterface by creating all required objects etc. Should usually be called when the implementation is enabled.
+     */
     void setup();
 
+    /**
+     * @return The currently in use {@link ExpressionParser}
+     */
     ExpressionParser getExpressionParser();
 
+    /**
+     * @return The currently in use {@link MethodInvoker}
+     */
     MethodInvoker getMethodInvoker();
 
+    /**
+     * @return The currently in use {@link CommandManager}
+     */
     CommandManager getCommandManager();
 }
