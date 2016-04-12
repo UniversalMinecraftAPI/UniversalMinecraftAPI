9663eb105c5d7996e28b542788605f168773051e
Koen Vlaswinkel <koen@vlaswinkel.info>
2016-04-12 15:14:25 +0200

    Fix using wrong event system

--- sponge/src/main/java/com/koenv/jsonapi/sponge/SpongeJSONAPI.java
+++ sponge/src/main/java/com/koenv/jsonapi/sponge/SpongeJSONAPI.java
@@ -1,6 +1,5 @@
 package com.koenv.jsonapi.sponge;
 
-import com.google.common.eventbus.Subscribe;
 import com.koenv.jsonapi.JSONAPI;
 import com.koenv.jsonapi.JSONAPIProvider;
 import com.koenv.jsonapi.methods.APIMethod;
@@ -9,6 +8,7 @@ import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.args.GenericArguments;
 import org.spongepowered.api.command.spec.CommandSpec;
 import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.text.Text;
@@ -17,7 +17,7 @@ import org.spongepowered.api.text.Text;
 public class SpongeJSONAPI implements JSONAPIProvider {
     private JSONAPI jsonapi;
 
-    @Subscribe
+    @Listener
     public void onServerStart(GameStartedServerEvent event) {
         jsonapi = new JSONAPI(this);
         jsonapi.setup();
