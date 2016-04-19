962d0aa88cf2dd412add1b6d6a66ce7ed65c9f4e
Koen Vlaswinkel <koen@vlaswinkel.info>
2016-04-19 20:13:37 +0200

    Add documentation for the console stream

--- docgenerator/src/main/java/com/koenv/jsonapi/docgenerator/generator/StreamDocGenerator.java
+++ docgenerator/src/main/java/com/koenv/jsonapi/docgenerator/generator/StreamDocGenerator.java
@@ -6,6 +6,8 @@ import com.typesafe.config.ConfigFactory;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
@@ -15,10 +17,12 @@ import java.util.Map;
 
 public class StreamDocGenerator extends AbstractGenerator {
     private String streamName;
+    private Logger logger;
 
     public StreamDocGenerator(File rootDirectory, String streamName) {
         super(rootDirectory);
         this.streamName = streamName;
+        this.logger = LoggerFactory.getLogger(streamName);
     }
 
     @Override
@@ -39,6 +43,8 @@ public class StreamDocGenerator extends AbstractGenerator {
             if (config.hasPath("returns")) {
                 dataModel.put("returns", classResolver.resolve(config.getString("returns")));
             }
+        } else {
+            logger.warn("No configuration file found for stream " + streamName + " at " + streamFile.getPath());
         }
 
         template.process(dataModel, output);
